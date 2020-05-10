9b7834e796b409954df5ccf39c967efc49a4376a
hojarasca <miguelfeliped@gmail.com>
2020-05-09 21:02:49 -0300

    Small script fix.

--- actions/deploy-all.sh
+++ actions/deploy-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 CURRENT_DIR="$(dirname "$0")"
-source "$CURRENT_DIR/deploy-network.sh"
+source "$CURRENT_DIR/deploy-network.sh" || true # Should always try to deploy the next one.
 source "$CURRENT_DIR/deploy-application.sh"
